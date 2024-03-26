879fc9067da1572523f9ef3474a0602da86ec4fe
Endered <yy56ga10ve@gmail.com>
2024-03-27 00:27:14 +0900

    add nix derivation

--- default.nix
+++ default.nix
@@ -1,10 +1,32 @@
 {
   pkgs ? import <nixpkgs> {}
 }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [
+let
+  mozc-dictionary = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/google/mozc/master/src/data/emoticon/emoticon.tsv";
+    hash = "sha256-NmVYs4C+8H3aJoIskQDR76vuU595YbLGNj1GFMSnYsQ=";
+  };
+in
+pkgs.stdenv.mkDerivation {
+  name = "skk-emoticon-from-mozc";
+
+  src = ./.;
+
+  nativeBuildInputs = with pkgs; [
     cargo
     rustc
     rust-script
   ];
+
+  buildPhase = ''
+    export XDG_CACHE_HOME=$PWD/.cache
+    cat ${mozc-dictionary} \
+      | rust-script mozc-emoticon-converter.rs \
+      > skk-emoticon.utf8
+  '';
+
+  installPhase = ''
+    mkdir -p $out/share
+    cp skk-emoticon.utf8 $out/share/skk-emoticon.utf
+  '';
 }
